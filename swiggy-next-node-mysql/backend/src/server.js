10d06cf6afb208c3e9732ddf357571acc1abde29
gagan937 <gr9582730@gmail.com>
2026-07-15 12:27:06 +0530

    Update server.js

--- swiggy-next-node-mysql/backend/src/server.js
+++ swiggy-next-node-mysql/backend/src/server.js
@@ -14,7 +14,7 @@ const PORT = process.env.PORT || 5000;
 // Security and Middleware
 app.use(helmet());
 app.use(cors({ 
-    origin: process.env.FRONTEND_URL || "http://localhost:3000", 
+    origin: process.env.FRONTEND_URL || "https://swiggy.techmng.online", 
     credentials: true 
 }));
 app.use(express.json({ limit: "1mb" }));
